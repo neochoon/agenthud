b3d294e4ec0a1e807949bd5040861f2decc69719
Choonsik Cho <neochoon@gmail.com>
2026-05-16 10:11:12 +1000

    feat: wire summary mode in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import React from "react";
 import { clearScreen, getHelp, getVersion, parseArgs } from "./cli.js";
 import { loadGlobalConfig } from "./config/globalConfig.js";
 import { generateReport } from "./data/reportGenerator.js";
+import { runSummary } from "./data/summaryRunner.js";
 import { discoverSessions } from "./data/sessions.js";
 import { App } from "./ui/App.js";
 
@@ -67,6 +68,20 @@ if (options.mode === "report") {
   });
   process.stdout.write(`${markdown}\n`);
   process.exit(0);
+}
+
+if (options.mode === "summary") {
+  if (options.summaryError) {
+    process.stderr.write(`agenthud: ${options.summaryError}\n`);
+    process.exit(1);
+  }
+  const exitCode = await runSummary({
+    date: options.summaryDate!,
+    prompt: options.summaryPrompt,
+    force: options.summaryForce ?? false,
+    today: new Date(),
+  });
+  process.exit(exitCode);
 }
 
 if (options.mode === "watch") {
